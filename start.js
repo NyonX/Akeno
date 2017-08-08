992a23ea86d4c941d3d428113ddd5c189d5ab575
NyonX <JaydenB0@protonmail.ch>
2017-08-08 01:47:30 -0700

    AI

--- start.js
+++ start.js
@@ -18,32 +18,59 @@ var owner = process.env.owner;
 var ai = apiai(process.env.ai);
 var prefix = process.env.prefix;
 
+//Log in Event 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Serving ${client.guilds.array().length} server(s).`);
+});
+
 // --------------------
 //---AI Integeration---
 //---------------------
+
 client.on("message", msg => {
-	if (msg.author.bot) return;
-	if(!msg.content.startsWith(prefix)) return;
-	console.log(msg);
+  
+  //Checks for prefix and bot
+if(!msg.content.startsWith(prefix)) return;
+if(msg.author.bot) return; 
 
-
-var request = app.textRequest(`${msg.content.slice(1)}`);
+let request = ai.textRequest(msg.content.slice(1), {sessionId: `${msg.author.id}`});
 
 // Log all responses.
 request.on('response', function(response) {
-    console.log(response);    
+    console.log(response);     
 });
- 
+
+//Log all errors
 request.on('error', function(error) {
     console.log(error);
 });
 
-request.end();
-	if (msg.content.startsWith(prefix + "")) {
-		ai.textRequest(`${msg.content.slice(1)}`);
-		request.on('response', function(response) {
-			let reply = response.result.fulfillment.speech;
-			client.sendMessage(msg, `${reply}`);
-		});
-	};
- });
+ 
+request.end()
+
+//Call API with message and reply back, then execute action
+   if (msg.content.startsWith(prefix + "")) {
+       ai.textRequest(`${msg.content.slice(1)}`, {sessionId: `${msg.author.id}`});
+       request.on('response', function(response) {
+       let responseText = response.result.fulfillment.speech;
+       if (response.result.action !== ''){
+       	let action = response.result.action;
+       	let actionSubject = response.result.parameters.Username;
+       	return;
+       }
+       msg.reply(`${responseText}`);
+    });
+    
+  }
+
+//Logs more errors
+request.on('error', function(error) {
+    console.log(error);
+
+
+});
+});
+
+//Login
+client.login(token);
